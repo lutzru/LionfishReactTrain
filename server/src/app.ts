03386fb43b6dc361766e1c5311efcad16cf424bc
lutzru <lutz.ruehmann@gmx.de>
2021-01-06 10:27:12 +0100

    kleine addons, json geparst,..

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,13 +8,42 @@ const port = 3000
 // All files in the "public" directory will be statically delivered
 app.use(express.static('public'))
 
+// Hier, Json parsen, global verfügbar
+app.use(express.json())
+
+// http://localhost:3000/api/hello2/DerLutz
+app.get('/api/hello2/:user', (req, res) => {
+    let u = req.params.user;
+    console.log(req)
+    res.json({ message: `Hello ${u} awesome world` })
+    logger.info('/api/hello called')
+})
+
+// http://localhost:3000/api/hello?user=DerLutz
 app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello, awesome world' })
+    let u = req.query.user;
+    console.log(req)
+    res.json({ message: `Hello ${u} awesome world` })
+    logger.info('/api/hello called')
+})
+
+// BodyParser, JSON zerlegen
+app.post('/api/login', (req, res) => {
+
+
+    let u = req.body.username
+    let p = req.body.password
+
+    console.log(req)
+    
+    res.json({ message: `Logindata: ${u} ${p} awesome world` })
     logger.info('/api/hello called')
 })
 
 // Create an endpoint where a react application can request a config that contains API urls etc. that depend on the environment
 app.get('/config.js', getConfig)
+
+console.log(process.env)
 
 // Start the server
 app.listen(port, () => {
