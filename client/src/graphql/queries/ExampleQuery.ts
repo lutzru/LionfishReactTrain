d6458a01682044be2a408d1c769e66050659e457
lutzru <lutz.ruehmann@gmx.de>
2021-02-01 16:50:53 +0100

    Example Querries added

--- client/src/graphql/queries/ExampleQuery.ts
+++ client/src/graphql/queries/ExampleQuery.ts
@@ -5,3 +5,81 @@ export default gql`
         exampleQuery
     }
 `
+
+/*
+
+
+
+http://localhost:4000/graphql
+
+GraphQL:
+# Write your query or mutation here
+{
+  anlieferungen {
+    id,lieferant{id,lieferantenNummer}
+  }
+}
+
+
+# Write your query or mutation here
+{
+  anlieferungen(searchTerm: "noch") {
+    id
+    lieferant {
+      id
+      lieferantenNummer
+      name
+    }
+    artikel {
+      name
+    }
+  }
+}
+
+
+# Write your query or mutation here
+{
+  anlieferungen(searchTerm: "") {
+    id
+    lieferant {
+      id
+      lieferantenNummer
+      name
+    }
+    artikel {
+      name
+    }
+  }
+   anlieferung(id:"125") {
+    id
+    lieferant {
+      id
+      lieferantenNummer
+      name
+    }
+    artikel {
+      name
+    }
+  }
+}
+
+
+{
+  anlieferungen(status: [OFFEN, FERTIG]) {
+    id
+    status
+  }
+}
+# Write your query or mutation here
+
+
+{
+  anlieferungen(status: [OFFEN, FERTIG],searchTerm:"noch") {
+    id
+    status,
+    lieferant{name},
+    artikel{articleNumber,name}
+  }
+}
+
+*/
